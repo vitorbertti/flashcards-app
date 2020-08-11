54832fd829b7ac83bbe072903cbdeb5259323ef1
Vitor Bertti <berttivitor@gmail.com>
2020-08-11 12:00:57 -0300

    Added bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
+    <title>Flashcards</title>
+</head>
+
+<body>
+
+    <h1>testando bootstrap</h1>
+
+    <button class="btn btn-large btn-success">Opa</button>
+
+    <script src="{{ asset('site/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap.js') }}"></script>
+
+</body>
+
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass("resources/views/scss/style.scss", "public/site/style.css")
+    .scripts("node_modules/jquery/dist/jquery.js", "public/site/jquery.js")
+    .scripts(
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+        "public/site/bootstrap.js"
+    );
